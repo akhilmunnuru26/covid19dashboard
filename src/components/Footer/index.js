63f76854b34a7556b237570454f126a0f15231a1
Akhil Munnuru <104784452+akhilmunnuru26@users.noreply.github.com>
2022-11-20 18:27:04 +0530

    Updated Code

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,15 +1,18 @@
-import './index.css'
-
+import {Link} from 'react-router-dom'
 import {VscGithubAlt} from 'react-icons/vsc'
 import {FiInstagram} from 'react-icons/fi'
 import {FaTwitter} from 'react-icons/fa'
+import './index.css'
 
 export default function Footer() {
   return (
     <div className="footer-page">
-      <h1 className="footer-logo">
-        COVID19<span className="span-logo">INDIA</span>
-      </h1>
+      <Link to="/" className="links">
+        <div className="footer-logo">
+          <span className="app-logo"> COVID19</span>
+          <span className="span-logo">INDIA</span>
+        </div>
+      </Link>
       <p className="motto">
         we stand with everyone fighting on the front lines
       </p>

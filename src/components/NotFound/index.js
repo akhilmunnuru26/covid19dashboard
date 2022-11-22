3c9a97909fd2279f479721fc3d3a66a27242efbd
Akhil Munnuru <104784452+akhilmunnuru26@users.noreply.github.com>
2022-11-22 20:34:11 +0530

    latest updated code

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -6,19 +6,18 @@ const NotFound = () => (
     <div className="not-found-content">
       <img
         src="https://res.cloudinary.com/dstuhdad3/image/upload/v1668418611/Group_7485_zfvjsi.png"
-        alt="covid error"
+        alt="not-found-pic"
         className="covid-error-image"
       />
       <h1 className="not-found-msg">Page Not Found</h1>
       <p className="not-found-description">
-        we’re sorry, the page you requested could not be found Please go back to
-        the homepage
+        we are sorry, the page you requested could not be found
       </p>
-      <button type="button" className="home-btn">
-        <Link className="links" to="/">
+      <Link className="links" to="/">
+        <button type="button" className="home-btn">
           Home
-        </Link>
-      </button>
+        </button>
+      </Link>
     </div>
   </div>
 )
